67a4d12a005b23932b3c6be0832c5686de820e76
Elie Ben-Shlomo <elieben-shlomo@Elies-MacBook-Pro.local>
2019-07-02 19:58:02 +0100

    Writing a mergesort

---
+++ problems/search-sort-problems/merge-sort.ts
@@ -0,0 +1,50 @@
+function merge<A>(s: A[], t: A[]): A[] {
+    var sTemp = s.slice()
+    var tTemp = t.slice()
+
+    const tempArr = []
+    const totalLength = s.length + t.length
+    while (tempArr.length < totalLength) {
+      if (sTemp.length == 0 && tTemp.length != 0) {
+        const head = tTemp[0]
+        tempArr.push(head)
+        tTemp.splice(0,1)
+      } else if (sTemp.length != 0 && tTemp.length == 0) {
+        const head = sTemp[0]
+        tempArr.push(head)
+        sTemp.splice(0,1)
+      } else {
+        if (sTemp[0] <= tTemp[0]) {
+          const head = sTemp[0]
+          tempArr.push(head)
+          sTemp.splice(0,1)
+        } else {
+          const head = tTemp[0]
+          tempArr.push(head)
+          tTemp.splice(0,1)
+        }
+      }
+    }
+    
+    return tempArr
+}
+
+function mergeSort<A>(arr: A[]): A[] {
+  if (arr.length == 1) {
+    return arr
+  }
+  
+
+  var tempArr = arr.slice()
+  const middleIndex = Math.ceil(tempArr.length / 2) 
+  const leftArr = tempArr.splice(0,middleIndex)
+  tempArr = arr.slice()
+  const rightArr = tempArr.splice(middleIndex)
+
+  const left = (mergeSort(leftArr))
+  const right = (mergeSort(rightArr))
+  
+  return merge(left, right)
+}
+
+console.log(mergeSort([2, 5, 3, 1, 4, 7, 10, 15,-23,321,0,100]))
