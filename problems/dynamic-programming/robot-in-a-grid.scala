db7e379ca1cad7a44486b5b35df33c96d9c91da8
Elie Ben-Shlomo <elieben-shlomo@Elies-MacBook-Pro.local>
2019-08-10 22:09:43 +0100

    Recursively solving robot-grid problem

--- problems/dynamic-programming/robot-in-a-grid.scala
+++ problems/dynamic-programming/robot-in-a-grid.scala
@@ -6,22 +6,28 @@ object Main {
   private val rows = 4
   private val columns = 3
   private val holes : List[(Int, Int)] = List(
-    (4,1), (2,1), (2,2)
+    (0,1), (2,1)
   )
   type Location = (Int, Int)
   type Path = List[Location]
 
   def main(args: Array[String]) = {
-    val initialPath = List((0,0))
-    val newPaths = expandPaths(List(initialPath))
+    val initialPaths = List(List((0,0)))
 
-    println(newPaths)
+    println(findPaths(initialPaths, 5))
   }
 
+  def findPaths(paths: List[Path], depth: Int): List[Path] =
+    if (depth == 0) paths
+    else findPaths(expandPaths(paths), depth - 1)
+
   def expandPaths(paths: List[Path]): List[Path] = {
-    val pathsWithHeads = paths.map(path => (path, path.head))
-    val pathsWithNeighbors = pathsWithHeads.map{case (path, head) => (path, neighbors(head))}
-    val pathsWithValidNeighbors = pathsWithNeighbors.map{case (path, neighbors) => (path, neighbors.filter(!holes.contains(_)))}
+    val pathLength = paths.head.length
+
+    val pathsWithHeads = paths.map(path => (path, path.last))
+    val pathsWithNeighbors = pathsWithHeads.map{case (path, last) => (path, neighbors(last))}
+
+    val pathsWithValidNeighbors = pathsWithNeighbors.map{case (path, neighbors) => (path, neighbors.filter(validLocation(_ , holes, rows, columns)))}
 
     val newPaths: List[Path] = pathsWithValidNeighbors.flatMap{
       case (path, neighbors) => neighbors.map(validNeighbor =>
@@ -29,9 +35,11 @@ object Main {
       )
     }
 
-    newPaths
+    newPaths.filter(_.length == pathLength + 1)
   }
 
+  def validLocation(location: Location, holes: List[Location], rows: Int, columns: Int): Boolean =
+    !holes.contains(location) && location._1 < rows && location._2 < columns
 
   def neighbors(location: Location): List[Location] = {
     List(location + (0,1), location + (1,0))
