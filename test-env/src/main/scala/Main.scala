a7e2291d83a695d3d08815582fad834bccf10f3b
Elie Ben-Shlomo <elieben-shlomo@Elies-MacBook-Pro.local>
2019-08-15 11:20:41 +0100

    working on longest substring problem

--- test-env/src/main/scala/Main.scala
+++ test-env/src/main/scala/Main.scala
@@ -1,52 +1,34 @@
-// Add two numbers (Linked List)
-// Unfinished
-
-class ListNode(var _x: Int = 0) {
-   var next: ListNode = null
-   var x: Int = _x
-}
+import scala.collection.mutable.ArrayBuffer
 
 object Main {
 
   def main(args: Array[String]) = {
-    var l1 = new ListNode(2)
-    l1.next = new ListNode(4)
-    l1.next.next = new ListNode(3)
-
-    var l2 = new ListNode(5)
-    l2.next = new ListNode(6)
-    l2.next.next = new ListNode(8)
-    l2.next.next.next = new ListNode(9)
-
-    var sum = addTwoNumbers(l1, l2, false)
-
-    println(sum._x)
-    println(sum.next._x)
-    println(sum.next.next._x)
-    println(sum.next.next.next._x)
-    println(sum.next.next.next.next._x)
+    println(lengthOfLongestSubstring(""))
   }
 
+  def lengthOfLongestSubstring(s: String): Int = {
+    val charList = s.toList
+    val hashMap = toHashMap(charList)
+    println(hashMap)
+    val listOfLists = hashMap.keys.toList.map(char => maxDifference(hashMap(char)))
+    println(listOfLists)
+    if (listOfLists.nonEmpty) listOfLists.max else 0
+  }
 
-  def addTwoNumbers(l1: ListNode, l2: ListNode, carryOn: Boolean): ListNode = {
-    val x1 = l1._x
-    val x2 = l2._x
-    val sum = x1 + x2 + (if (carryOn) 1 else 0)
+  def toHashMap(list: List[Char]): Map[Char, List[Int]] = {
+    list.zipWithIndex.foldLeft(Map.empty[Char, List[Int]])((map, e) => map + (e._1 -> (map.getOrElse(e._1, List()) ++ List(e._2))))
+  }
 
-    val nextCarryOn = if (sum >= 10) true else false
-    val sumDigit = if (!nextCarryOn) sum else sum - 10
-
-    var node = new ListNode(sumDigit)
-
-    if (l1.next == null && l2.next == null) {
-      if (carryOn) return new ListNode(0)
-      else return node
+  def maxDifference(list: List[Int]): Int = {
+    if (list.length == 0) return 0
+    var diff = 0
+    for (i <- 1 until list.length) {
+      val newDiff = Math.abs(list(i) - list(i - 1))
+      if (newDiff > diff) {
+        diff = newDiff
+      }
     }
-
-    val l1Next = if (l1.next != null) l1.next else new ListNode(0)
-    val l2Next = if (l2.next != null) l2.next else new ListNode(0)
-
-    node.next = addTwoNumbers(l1Next, l2Next, nextCarryOn)
-    node
+    diff
   }
+
 }
